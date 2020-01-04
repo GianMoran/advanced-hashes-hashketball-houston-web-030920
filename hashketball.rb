191ebba2dd672451d319fba70d04999e69dedfa7
Gian Moran <gpmoran_@outlook.com>
2020-01-04 23:29:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -246,7 +246,6 @@ def most_points_scored
     team_name.each do|team_attributes,team_data|
       if team_attributes == :players
         team_data.each do |players|
-          binding.pry
           if players[:points] > most_points_scored
                most_points_scored = players[:points]
               player = players[:player_name]
